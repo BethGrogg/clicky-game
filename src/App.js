bf5a133821af6264c1930e8cf649933c49817d8d
Bethany <bethany.grogg@gmail.com>
2019-08-09 09:08:41 -0400

    score is updating

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pictures from "./pictures.json";
 import PictureCard from "./components/PictureCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import Score from "./components/Score";
 
 function shuffleArray(array) {
   let i = array.length - 1;
@@ -17,18 +18,21 @@ function shuffleArray(array) {
 }
 class App extends Component {
 state = {
-  pictures
+  pictures,
+  selectedPicturesArray: [],
+  currentScore: 0
 };
 
 
 
 selectPicture = id => {
-console.log("in here");
-  if (this.state.clicked === true) {
+let selectedPicturesArray = this.state.selectedPicturesArray;
+  if(selectedPicturesArray.includes(id)) {
     alert("Sorry, you lose!");
   }
     else {
-      this.clicked = true;
+      selectedPicturesArray.push(id);
+      this.setState({currentScore: selectedPicturesArray.length});
       shuffleArray(this.state.pictures);
       this.setState({ pictures });
     }
@@ -39,13 +43,14 @@ render() {
   return (
     <Wrapper>
       <Title>Clicky Game</Title>
+      <Score>Your Score: {this.state.currentScore}
+      </Score>
       {shuffledPictures.map(picture => (
         <PictureCard
         selectPicture = {this.selectPicture}
           id={picture.id}
           key={picture.id}
           image={picture.image}
-          clicked={this.clicked}
         />
         ))};
     </Wrapper>
