babb3abbbfb1116f108871c16b98f1936faa36fa
Bethany <bethany.grogg@gmail.com>
2019-08-10 12:38:54 -0400

    Updates

---
+++ precache-manifest.11709c9cb4faee9b3226a615dc985b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db343949b677c1f35037a1f7dc5cb06",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6a3deb580ce52ae4ff9a",
+    "url": "/clicky-game/static/css/main.30460c2a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "6a3deb580ce52ae4ff9a",
+    "url": "/clicky-game/static/js/main.71161b7d.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
