088d017e1c1a849021c7123fbd671b9eb9f4da3e
Bethany <bethany.grogg@gmail.com>
2019-08-10 07:57:50 -0400

    fixed ui--images

--- src/components/PictureCard/index.js
+++ src/components/PictureCard/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./style.css";
 
+var imageStyle = {
+  width: '325px',
+  height: '325px',
+  objectFit: 'cover'
+}
+
 function PictureCard(props) {
   return (
     <div className="card">
       <span onClick={() => props.selectPicture(props.id)} className="select">
       <div className="img-container">
-        <img alt={props.id} src={props.image} />
+        <img alt={props.id} src={props.image} style={imageStyle}/>
       </div>
       
        
